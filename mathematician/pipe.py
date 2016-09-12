8168909cb85825a10874dbec194ece3601c48820
unHealthy <zhutian.chen@outlook.com>
2016-09-12 15:32:59 +0800

    refine pipe class

--- mathematician/pipe.py
+++ mathematician/pipe.py
@@ -12,7 +12,7 @@ class PipeModule(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def process(self, raw_data):
+    def process(self, raw_data, raw_data_files = None):
         """Required.Process the raw data and return the processed data
 
         Args:
@@ -21,8 +21,7 @@ class PipeModule(metaclass=ABCMeta):
         Returns:
             dict: The return value, processed data.
         """
-        processed_data = raw_data
-        return processed_data
+        pass
 
 
 class Pipe:
@@ -35,31 +34,60 @@ class Pipe:
     """
 
     def __init__(self):
-        self.__raw_data = None
+        self.__raw_data_files = []
+        # self.__raw_data = None
         self._processed_data = None
+        self._processors = []
 
-    def input(self, data):
-        """
-            input raw data for processing
-        """
+    # def input(self, data):
+    #     """
+    #         input raw data for processing
+    #     """
 
-        self.__raw_data = {
-            "created_date": datetime.now(),
-            "data": data
-        }
-        # in the beginning both raw data and processed data are the same
-        self._processed_data = self.__raw_data
+    #     self.__raw_data = {
+    #         "created_date": datetime.now(),
+    #         "data": data
+    #     }
+    #     # in the beginning both raw data and processed data are the same
+    #     self._processed_data = self.__raw_data
+    #     return self
+
+    def input_file(self, filename):
+        if type(filename) is not str or len(filename) == 0:
+            raise TypeError('filename should be a non-empty str')
+        self.__raw_data_files.append(filename)
+        return self
+
+    def input_files(self, filenames):
+        if type(filenames) is not list:
+            raise TypeError('filenames should be a non-empty list')
+        self.__raw_data_files += filenames
         return self
 
     def pipe(self, processor):
         """
             Chain the module one by one so that they will run sequentially
         """
-        if isinstance(processor, PipeModule) is False and self._processed_data is None:
+        if isinstance(processor, PipeModule) is False:
             return None
+        self._processors.append(processor)
+        return self
 
-        self._processed_data = processor.process(self._processed_data)
+    def concat(self, pipeline):
+        """
+            Concatenate another pipeline to this pipeline
+        """
         return self
 
     def output(self):
+        """
+            Excute all processor one by one and return the data after processing
+        """
+        if self._processed_data is not None:
+            self._processed_data = {'created_date': datetime.now(), 'data': {}}
+
+            for processor in self._processors:
+                self._processed_data = processor.process(self._processed_data, self.__raw_data_files)
+
+                self._processed_data['finished_date'] = datetime.now()
         return self._processed_data
