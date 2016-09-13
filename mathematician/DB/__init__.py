c0a8333ad046ae2610050b5fcca3d5d6405a23c5
shaozu <shaozu.cao@gmail.com>
2016-09-13 18:41:10 +0800

    convert config file to class

--- mathematician/DB/__init__.py
+++ mathematician/DB/__init__.py
@@ -1,5 +1,6 @@
 from .base_dbhelper import BaseDB, BaseCollection
+from .dbconfig import DBConfig
 from . import mongo_dbhelper
 from . import dbcreater
 
-__all__ = ["BaseDB", "BaseCollection", "mongo_dbhelper"]
+__all__ = ["BaseDB", "BaseCollection", "mongo_dbhelper", "DBConfig"]
