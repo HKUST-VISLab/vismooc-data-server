09253b4d862dcbb4086ae14553f33aee00b1a352
unHealthy <zhutian.chen@outlook.com>
2016-09-16 13:34:34 +0800

    rename to config.py

--- mathematician/DB/__init__.py
+++ mathematician/DB/__init__.py
@@ -1,5 +1,4 @@
 from .base_dbhelper import BaseDB, BaseCollection
 from . import mongo_dbhelper, mongo_dbcreator
-from .dbconfig import DBConfig
 
-__all__ = ["BaseDB", "BaseCollection", "mongo_dbhelper", "mongo_dbcreator","DBConfig"]
+__all__ = ["BaseDB", "BaseCollection", "mongo_dbhelper", "mongo_dbcreator"]
