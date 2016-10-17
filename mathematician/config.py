7169b81dc9b1fa5d2d9f4da1a0369995d9a8d3f4
unHealthy <zhutian.chen@outlook.com>
2016-10-17 20:37:19 +0800

    fix some schema problem

--- mathematician/config.py
+++ mathematician/config.py
@@ -16,6 +16,7 @@ class DBConfig:
     INDEX_GENERAL_INDEX_ORDER = "order"
 
     COLLECTION_COURSE = "courses"
+    FIELD_COURSE_ORIGINAL_ID = "originalId"
     FIELD_COURSE_NAME = "name"
     FIELD_COURSE_YEAR = "year"
     FIELD_COURSE_INSTRUCTOR = "instructor"
@@ -24,17 +25,16 @@ class DBConfig:
     FIELD_COURSE_IMAGE = "image"
     FIELD_COURSE_DESCRIPTION = "description"
     FIELD_COURSE_METAINFO = "metaInfo"
-    FIELD_COURSE_ENDTIME = "endDate"
     FIELD_COURSE_STARTTIME = "startDate"
-    FIELD_COURSE_ORIGINAL_ID = "originalId"
+    FIELD_COURSE_ENDTIME = "endDate"
     FIELD_COURSE_STUDENT_LIST = "studentIds"
     FIELD_COURSE_VIDEO_LIST = "videoIds"
 
     COLLECTION_USER = "users"
+    FIELD_USER_ORIGINAL_ID = "originalId"
     FIELD_USER_NAME = "username"
     FIELD_USER_AGE = "age"
     FIELD_USER_GENDER = "gender"
-    FIELD_USER_ORIGINAL_ID = "originalId"
     FIELD_USER_COUNTRY = "country"
     FIELD_USER_COURSE_LIST = "courseIds"
     FIELD_USER_DROPPED_COURSE_LIST = "droppedCourseIds"
@@ -46,6 +46,7 @@ class DBConfig:
     FIELD_ENROLLMENT_ACTION = "action"
 
     COLLECTION_VIDEO = "videos"
+    FIELD_VIDEO_ORIGINAL_ID = "originalId"
     FIELD_VIDEO_COURSE_ID = "courseId"
     FIELD_VIDEO_NAME = "name"
     FIELD_VIDEO_TEMPORAL_HOTNESS = "temporalHotness"
@@ -55,15 +56,14 @@ class DBConfig:
     FIELD_VIDEO_RELEASE_DATE = "releaseDate"
     FIELD_VIDEO_URL = "url"
     FIELD_VIDEO_DURATION = "duration"
-    FIELD_VIDEO_ORIGINAL_ID = "originalId"
 
     COLLECTION_VIDEO_LOG = "videoLogs"
+    FIELD_VIDEO_LOG_ORIGINAL_ID = "originalId"
     FIELD_VIDEO_LOG_USER_ID = "userId"
     FIELD_VIDEO_LOG_VIDEO_ID = "videoId"
     FIELD_VIDEO_LOG_TIMESTAMP = "timestamp"
     FIELD_VIDEO_LOG_TYPE = "type"
     FIELD_VIDEO_LOG_METAINFO = "metaInfo"
-    FIELD_VIDEO_LOG_ORIGINAL_ID = "originalId"
 
     CONFIG_JSON = {
         DB_HOST: "localhost",
