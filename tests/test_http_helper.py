05f44045ea56a3d4c052f9aa3e446765f8165629
unHealthy <zhutian.chen@outlook.com>
2016-12-24 15:33:38 +0800

    improve the http_helper code

---
+++ tests/test_http_helper.py
@@ -0,0 +1,11 @@
+'''Unit test of http_helper module
+'''
+import unittest
+import mathematician.http_helper as http
+
+class TestHTTPHelperClass(unittest.TestCase):
+    '''Unit test of http_helper module
+    '''
+    def setUp(self):
+        pass
+    
