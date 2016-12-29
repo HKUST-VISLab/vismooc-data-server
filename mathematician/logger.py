8f315ffddd3d468c191e76dd0443a68f43560143
chenzhutian <zhutian.chen@outlook.com>
2016-12-29 18:59:47 +0800

    finish a text progress bar

--- mathematician/logger.py
+++ mathematician/logger.py
@@ -1,14 +1,40 @@
 '''Logger for vismooc data server
 '''
+import logging
+from datetime import datetime
+VISMOOC_LOGGER = logging.getLogger("vismooc")
+VISMOOC_LOGGER.setLevel(logging.INFO)
 
-PREFIX = "===vismooc===>"
-def warn(msg, end='\n'):
+CONSOLE_HANDLER = logging.StreamHandler()
+CONSOLE_HANDLER.setLevel(logging.INFO)
+
+FORMATTER = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+
+CONSOLE_HANDLER.setFormatter(FORMATTER)
+VISMOOC_LOGGER.addHandler(CONSOLE_HANDLER)
+
+def warn(msg):
     '''Print the Warning message from vismooc data server
     '''
-    print("[WARN]" + PREFIX + str(msg), end=end)
+    VISMOOC_LOGGER.warning(str(msg))
 
-
-def info(msg, end='\n'):
+def info(msg):
     '''Print the Info message from vismooc data server
     '''
-    print("[INFO]" + PREFIX + str(msg), end=end)
+    VISMOOC_LOGGER.info(str(msg))
+
+PROGRESS_LENGTH = 40
+def progressbar(filename, progress, total):
+    '''A text progress bar
+    '''
+    progress_length = int((int(progress) * PROGRESS_LENGTH)/ int(total))
+    time_now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    str_template = "\r%s-vismooc-%s-[%s%s] %d%% | %s"
+    # sys.stdout.write(str_template % (time_now, 'DOWNLOADING', '#' * progress_length,
+    #                                  ' ' * (PROGRESS_LENGTH - progress_length),
+    #                                  int(progress / total * 100), filename))
+    print(str_template % (time_now, 'DOWNLOADING', '#' * progress_length,
+                          ' ' * (PROGRESS_LENGTH - progress_length),
+                          int(progress / total * 100), filename), end='', flush=True)
+    if progress == total:
+        print('\n', end='', flush=True)
