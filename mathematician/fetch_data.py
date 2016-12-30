c7b6a39106698b7ff201b338cbc3a01819530ff2
chenzhutian <zhutian.chen@outlook.com>
2016-12-30 21:02:45 +0800

    improve a lot of fetch_data

--- mathematician/fetch_data.py
+++ mathematician/fetch_data.py
@@ -4,19 +4,18 @@
 import tarfile
 import gzip
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from . import http_helper as http
 from .DB import mongo_dbhelper
 from .config import DBConfig as DBC, DataSource as DS, ThirdPartyKeys as TPK, FilenameConfig as FC
 from .logger import warn, info
 
-
+FIELD_LAST_MODIFIED = "Last-Modified"
+FIELD_ETAG = "ETag"
+FIELD_MD5 = "Content-MD5"
+FIELD_CONTENT_TYPE = "Content-Type"
 class DownloadFileFromServer():
     """Download file from server"""
-    FILED_LAST_MODIFIED = "Last-Modified"
-    FIELD_ETAG = "ETag"
-    FIELD_MD5 = "Content-MD5"
-    FIELD_CONTENT_TYPE = "Content-Type"
 
     def __init__(self, save_dir, host=None, api_key=None, access_token=None):
         self.__api_key = api_key or TPK.HKMooc_key
@@ -95,35 +94,35 @@ class DownloadFileFromServer():
             except AttributeError as ex:
                 warn("In get_click_stream(), cannot get target attribute(`collection` or `items`)\
                      from the response")
-                print(ex)
+                warn(ex)
             file_urls = []
-            md5s = {}
             try:
                 for item in items:
                     href = item.get('href')
                     file_urls.append(href[href.index(DS.HOST) + len(DS.HOST):])
-                    md5s[href[href.rindex("/") + 1:]] = item.get('md5')
             except AttributeError as ex:
                 warn("In get_click_stream(), cannot get target attribute(`href` or `md5`) from\
                      items")
-                print(ex)
+                warn(ex)
 
             info('Begin to download log-files, totally ' +
                  str(len(file_urls)) + " files, please wait")
             downloaded_files = self.__http_connection.download_files(
-                file_urls, save_dir, common_suffix=FC.Clickstream_suffix)
+                file_urls, save_dir, common_suffix=FC.Clickstream_suffix, retry_time=1)
             info('Finish downloading log-fiels')
             info('Begin to decompress log-files')
-            self.decompress_files(downloaded_files, "gzip")
+            self.decompress_files([response.get_content() for response in downloaded_files], "gzip")
             info('Finish decompress log-files')
             # cache the metaInfo of log files into database
             info('Begin to cache the metainfo of log-files into mongoDB')
             new_meta_items = [{
                 DBC.FIELD_METADBFILES_CREATEDAT: now,
-                DBC.FIELD_METADBFILES_FILEPATH: file_path,
-                DBC.FIELD_METADBFILES_ETAG: md5s.get(file_path[file_path.rindex(os.sep) + 1:]),
-                DBC.FIELD_METADBFILES_TYPE: DBC.TYPE_CLICKSTREAM
-            } for file_path in downloaded_files if os.path.exists(file_path)]
+                DBC.FIELD_METADBFILES_FILEPATH: res.get_content(),
+                DBC.FIELD_METADBFILES_ETAG: res.get_headers().get(FIELD_MD5),
+                DBC.FIELD_METADBFILES_TYPE: DBC.TYPE_CLICKSTREAM,
+                DBC.FIELD_METADBFILES_LAST_MODIFIED: res.get_headers().get(FIELD_LAST_MODIFIED),
+                DBC.FIELD_METADBFILES_PROCESSED: False,
+            } for res in downloaded_files if os.path.exists(res.get_content())]
             # if no log has been downloaded, add a empty log record
             if len(new_meta_items) > 1:
                 self._db.get_collection(DBC.COLLECTION_METADBFILES).insert_many(new_meta_items)
@@ -135,48 +134,47 @@ class DownloadFileFromServer():
         else:
             warn("In get_click_stream(), the return code of server is not 200")
 
-    # def get_mongo_snapshot(self, save_dir=None):
-    #     '''Get the mongo snapshot
-    #     '''
-    #     self.set_token()
-    #     now = int(datetime.now().timestamp())
-    #     save_dir = save_dir or self.__save_dir
-    #     new_meta_items = []
+    def get_mongo_snapshot(self, save_dir=None):
+        '''Get the mongo snapshot
+        '''
+        self.set_token()
+        now = int(datetime.now().timestamp())
+        save_dir = save_dir or self.__save_dir
+        new_meta_items = []
 
-    #     # check what file should be downloaded
-    #     info("Begin to analyze which snapshots need to be downloaded")
-    #     urls = []
-    #     md5s = []
+        # check what file should be downloaded
+        info("Begin to analyze which snapshots need to be downloaded")
+        urls = []
+        md5s = []
 
-    #     mysql_header = self.__http_connection.head(DS.SQLDB_URL).get_headers()
-    #     mysql_md5 = mysql_header.get(DownloadFileFromServer.FIELD_MD5)
-    #     mysql_etag = mysql_header.get(DownloadFileFromServer.FIELD_ETAG)
-    #     mysql_last_modified = mysql_header.get(DownloadFileFromServer.FILED_LAST_MODIFIED)
-    #     if mysql_etag and mysql_etag not in self.__metainfo_downloaded.keys():
-    #         urls.append(DS.SQLDB_URL)
-    #         md5s.append(mysql_md5)
+        mysql_header = self.__http_connection.head(DS.SQLDB_URL).get_headers()
+        mysql_md5 = mysql_header.get(FIELD_MD5)
+        mysql_etag = mysql_header.get(FIELD_ETAG)
+        mysql_last_modified = mysql_header.get(FIELD_LAST_MODIFIED)
+        if mysql_etag and mysql_etag not in self.__metainfo_downloaded.keys():
+            urls.append(DS.SQLDB_URL)
+            md5s.append(mysql_md5)
 
-    #     info("Begin to download DB snapshots, totally " + str(len(urls)) + " files, pleas wait")
-    #     downloaded_files = self.__http_connection.download_file(urls[0], save_dir, md5_checksum=md5s[0])
-    #     info("Finish download DB snapshots. The files we downloaded is "+",".join(downloaded_files))
-    #     for file_path in downloaded_files:
-    #         if FC.SQLDB_FILE in file_path:
-    #             info("Begin to decompress the mysql snapshot")
-    #             self.decompress_files([file_path, ], "gzip")
-    #             info("Finish decompressing the mysql snapshot")
-    #             # cache the file info if  it has been downloaded successfully
-    #             item = {
-    #                 DBC.FIELD_METADBFILES_CREATEDAT: now,
-    #                 DBC.FIELD_METADBFILES_LAST_MODIFIED: mysql_last_modified,
-    #                 DBC.FIELD_METADBFILES_ETAG: mysql_etag,
-    #                 DBC.FIELD_METADBFILES_FILEPATH: os.path.join(save_dir, FC.SQLDB_FILE),
-    #                 DBC.FIELD_METADBFILES_TYPE: DBC.TYPE_MYSQL
-    #             }
-    #             new_meta_items.append(item)
-    #     info('Begin to cache the metainfo of dbsnapshots into mongoDB')
-    #     self._db.get_collection(DBC.COLLECTION_METADBFILES).insert_many(new_meta_items)
-    #     info('Finish caching the metainfo of dbsnapshots into mongoDB')
-    #     return new_meta_items
+        info("Begin to download DB snapshots, totally " + str(len(urls)) + " files, pleas wait")
+        file_path = self.__http_connection.download_file(urls[0], save_dir, md5_checksum=md5s[0])
+        info("Finish download DB snapshots. The files we downloaded is " + file_path)
+        if FC.SQLDB_FILE in file_path:
+            info("Begin to decompress the mysql snapshot")
+            self.decompress_files([file_path, ], "gzip")
+            info("Finish decompressing the mysql snapshot")
+            # cache the file info if  it has been downloaded successfully
+            item = {
+                DBC.FIELD_METADBFILES_CREATEDAT: now,
+                DBC.FIELD_METADBFILES_LAST_MODIFIED: mysql_last_modified,
+                DBC.FIELD_METADBFILES_ETAG: mysql_etag,
+                DBC.FIELD_METADBFILES_FILEPATH: os.path.join(save_dir, FC.SQLDB_FILE),
+                DBC.FIELD_METADBFILES_TYPE: DBC.TYPE_MYSQL
+            }
+            new_meta_items.append(item)
+        info('Begin to cache the metainfo of dbsnapshots into mongoDB')
+        self._db.get_collection(DBC.COLLECTION_METADBFILES).insert_many(new_meta_items)
+        info('Finish caching the metainfo of dbsnapshots into mongoDB')
+        return new_meta_items
 
     def get_mongo_and_mysql_snapshot(self, save_dir=None):
         """ Download mongodb and mysql snapshot
@@ -189,27 +187,27 @@ class DownloadFileFromServer():
         # check what file should be downloaded
         info("Begin to analyze which snapshots need to be downloaded")
         urls = []
-        md5s = []
         mongo_header = self.__http_connection.head(DS.MONGODB_URL).get_headers()
-        mongo_md5 = mongo_header.get(DownloadFileFromServer.FIELD_MD5)
-        mongo_etag = mongo_header.get(DownloadFileFromServer.FIELD_ETAG)
-        mongo_last_modified = mongo_header.get(DownloadFileFromServer.FILED_LAST_MODIFIED)
+        # mongo_md5 = mongo_header.get(FIELD_MD5)
+        mongo_etag = mongo_header.get(FIELD_ETAG)
+        mongo_last_modified = mongo_header.get(FIELD_LAST_MODIFIED)
         if mongo_etag and mongo_etag not in self.__metainfo_downloaded.keys():
             urls.append(DS.MONGODB_URL)
-            md5s.append(mongo_md5)
 
         mysql_header = self.__http_connection.head(DS.SQLDB_URL).get_headers()
-        mysql_md5 = mysql_header.get(DownloadFileFromServer.FIELD_MD5)
-        mysql_etag = mysql_header.get(DownloadFileFromServer.FIELD_ETAG)
-        mysql_last_modified = mysql_header.get(DownloadFileFromServer.FILED_LAST_MODIFIED)
+        # mysql_md5 = mysql_header.get(FIELD_MD5)
+        mysql_etag = mysql_header.get(FIELD_ETAG)
+        mysql_last_modified = mysql_header.get(FIELD_LAST_MODIFIED)
         if mysql_etag and mysql_etag not in self.__metainfo_downloaded.keys():
             urls.append(DS.SQLDB_URL)
-            md5s.append(mysql_md5)
 
         info("Begin to download DB snapshots, totally " + str(len(urls)) + " files, pleas wait")
-        downloaded_files = self.__http_connection.download_files(urls, save_dir, md5_checksums=md5s)
+        downloaded_files = self.__http_connection.download_files(urls, save_dir)
         info("Finish download DB snapshots. The files we downloaded is "+",".join(downloaded_files))
-        for file_path in downloaded_files:
+        if len(urls) == 0:
+            return new_meta_items
+        for response in downloaded_files:
+            file_path = response.get_content()
             if FC.MongoDB_FILE in file_path:
                 info("Begin to decompress the mongo snapshot")
                 self.decompress_files([file_path, ], "gtar")
@@ -224,7 +222,7 @@ class DownloadFileFromServer():
                     DBC.FIELD_METADBFILES_PROCESSED: False
                 }
                 new_meta_items.append(item)
-            if FC.SQLDB_FILE in file_path:
+            elif FC.SQLDB_FILE in file_path:
                 info("Begin to decompress the mysql snapshot")
                 self.decompress_files([file_path, ], "gzip")
                 info("Finish decompressing the mysql snapshot")
@@ -244,7 +242,6 @@ class DownloadFileFromServer():
         return new_meta_items
 
     def decompress_files(self, file_paths, compress_algorithm):
-        # TODO try to optimize this part of code, add Exception handler
         """ This method is to verify and decompress file using specified algorithm.
             Use md5 to verify files' data intergrity.
             The parameter `file_md5` is a dict in which key is file name you want
@@ -257,26 +254,35 @@ class DownloadFileFromServer():
             if not os.path.exists(file_path):
                 continue
             if compress_algorithm == "gzip":
+                chunk_size = 1 << 16
                 with open(file_path, 'rb') as file:
-                    file_data = file.read()
-                with open(file_path, 'wb') as file:
-                    decompass_data = gzip.decompress(file_data)
-                    file.write(decompass_data)
+                    new_file_path = file_path[:file_path.rindex('.')]
+                    file_out = gzip.open(new_file_path, "wb")
+                    while True:
+                        chunk = file.read(chunk_size)
+                        if not chunk:
+                            break
+                        file_out.write(chunk)
+                    file_out.close()
+                    os.remove(file_path)
             elif compress_algorithm == "gtar":
-                with tarfile.open(file_path, 'r') as tar:
-                    tar_root = tar.getnames()[0]
-                    tar.extractall(path=os.path.dirname(file_path))
-                    self.bson2json(os.path.join(os.path.dirname(file_path), tar_root))
+                try:
+                    with tarfile.open(file_path, 'r') as tar:
+                        # tar_root = tar.getnames()[0]
+                        tar.extractall(path=os.path.dirname(file_path))
+                except tarfile.TarError as ex:
+                    warn("Extract file from mongodbsnapshot failed!")
+                    warn(ex)
+                # self.bson2json(os.path.join(os.path.dirname(file_path), tar_root))
 
-    def bson2json(self, dir_name):
-        # TODO try to optimize this part of code, add Exception handler
-        '''Convert all bson files under specific dir to json files
-        '''
-        file_to_be_process = set([FC.ACTIVE_VERSIONS[FC.ACTIVE_VERSIONS.rindex('/') + 1:FC.ACTIVE_VERSIONS.rindex('.')] + ".bson",
-                                  FC.STRUCTURES[FC.STRUCTURES.rindex('/') + 1:FC.STRUCTURES.rindex('.')] + ".bson"])
-        files = [os.path.join(dir_name, file) for file in os.listdir(dir_name)]
-        for file in files:
-            if os.path.isfile(file) and os.path.basename(file) in file_to_be_process:
-                cmd = "bsondump " + file + " > " + file[0:file.rindex('.') + 1] + 'json'
-                os.system(cmd)
-                # os.remove(file)
+    # def bson2json(self, dir_name):
+    #     '''Convert all bson files under specific dir to json files
+    #     '''
+    #     file_to_be_process = set([FC.ACTIVE_VERSIONS[FC.ACTIVE_VERSIONS.rindex('/') + 1:FC.ACTIVE_VERSIONS.rindex('.')] + ".bson",
+    #                               FC.STRUCTURES[FC.STRUCTURES.rindex('/') + 1:FC.STRUCTURES.rindex('.')] + ".bson"])
+    #     files = [os.path.join(dir_name, file) for file in os.listdir(dir_name)]
+    #     for file in files:
+    #         if os.path.isfile(file) and os.path.basename(file) in file_to_be_process:
+    #             cmd = "bsondump " + file + " > " + file[0:file.rindex('.') + 1] + 'json'
+    #             os.system(cmd)
+    #             # os.remove(file)
