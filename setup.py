ed570df9e7db274eb40a06b6afbb472858a431a1
unHealthy <zhutian.chen@outlook.com>
2017-01-05 19:11:27 +0800

    clean the requirement

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
+'''Setup script
+'''
 from setuptools import setup
 
 setup(
     name="mathematician",
-    version="0.0.1",
+    version="0.2.1",
     author="HKUST-VISlab",
     author_email="zhutian.chen@outlook.com",
     description="This is the dataserver of vismooc system",
     keywords="vismooc, dataserver, vislab, hkust",
     url="https://github.com/HKUST-VISLab/vismooc-data-server",
     packages=["mathematician"],
-    install_requires=[],
+    install_requires=[
+        "aiohttp>=0.22.5",
+        "pymongo>=3.3.0"
+    ],
     test_suite="tests.get_tests"
 )
