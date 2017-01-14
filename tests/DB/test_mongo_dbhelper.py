234c4493aa115a5309da77682e5d20c415e4cf53
zhutian <zhutian.chen@outlook.com>
2017-01-14 19:08:16 +0800

    test create collection

--- tests/DB/test_mongo_dbhelper.py
+++ tests/DB/test_mongo_dbhelper.py
@@ -1,20 +1,31 @@
 '''Test mongodb_helper module
 '''
 import unittest
-from unittest.mock import patch, MagicMock
-from mathematician.DB.mongo_dbhelper import MongoDB
+# from unittest.mock import patch, MagicMock
+from mathematician.DB.mongo_dbhelper import MongoDB, MongoCollection
+
 
 class TestMongoDB(unittest.TestCase):
     '''Test the MongoDb helper
     '''
 
+    def setUp(self):
+        host = "127.0.0.1"
+        db_name = "db"
+        port = 27017
+        self.mongodb = MongoDB(host, db_name, port)
+
     # @patch("mathematician.DB.mongo_dbhelper.MongoClient")
     def test_constructor(self):
         '''test the constructor of MongoDB
         '''
-        host = "127.0.0.1"
-        db_name = "db"
-        port = 27017
+        self.assertIsInstance(self.mongodb, MongoDB, "New a mongoDB instance")
 
-        mongodb = MongoDB(host, db_name, port)
-        self.assertIsInstance(mongodb, MongoDB, "New a mongoDB instance")
+    def test_create_collection(self):
+        '''test create a collection of a MongoDB
+        '''
+        name = "test_collection"
+        collection = self.mongodb.create_collection(name)
+        self.assertIsInstance(collection, MongoCollection, "Create a MongoCollection instance")
+        self.assertIn(name, self.mongodb.get_collection_names(),
+                      "New collection is created secessfully")
