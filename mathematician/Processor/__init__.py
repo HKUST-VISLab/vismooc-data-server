a05c8f30aa3f506f28bcc023fc8c1b0a0a79d5ed
zhutian chen <zhutian.chen@outlook.com>
2017-05-28 20:44:59 +0800

    fix a bug

--- mathematician/Processor/__init__.py
+++ mathematician/Processor/__init__.py
@@ -1,4 +1,5 @@
 '''Data processor
 '''
+from . import Rawfile2MongoProcessor, Sql2MongoProcessor, utils
 
-__All__ = ['Rawfile2MongoProcessor', 'Sql2MongoProcessor']
+__All__ = ['Rawfile2MongoProcessor', 'Sql2MongoProcessor', 'utils']
